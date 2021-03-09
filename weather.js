a48b5987cb303cfefcfdf26b372878be572823bd
Narastro <78056880+Narastro@users.noreply.github.com>
2021-03-09 21:31:38 +0900

    Demo-finished

---
+++ weather.js
@@ -0,0 +1,52 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = '6b8b1e9632c742fbbfa37feae848cc58';
+const COORDS = 'coords';
+
+function getWeather(lat,lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    })
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS,JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };       //객체 내에 key와 value를 갖게 지정할 때는 하나만 써도 됨!
+    saveCoords(coordsObj);
+    getWeather(latitude,longitude);
+}
+function handleGeoError(){
+    console.log("Can't access location")
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces,handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null){
+        askForCoords();
+    }else{
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+init();
